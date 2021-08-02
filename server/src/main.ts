800d40f06bd8f4aaac9fd6c23586b91c04a90797
Dmitrij <dmitrij.korev@convergent.digital>
2021-08-02 20:55:07 +0300

    first commit

---
+++ server/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+
+    app.listen(PORT, () => {
+      console.log(`Server is started on ${PORT} port`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
